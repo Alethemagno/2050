366d3625b51a8806420a2b02963b3069870dbd89
Alex Perez-Lucerga <alethemagno@gmail.com>
2021-01-03 11:07:17 +0100

    Addd item pickups

---
+++ 2050_4.16/Assets/InventoryThings/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public Inventory inventory;
+    public GameObject itemButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        Debug.Log(inventory);
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("It worked");
+            for (int i = 0; i < inventory.slots.Length; i++) {
+                if (!inventory.isFull[i]) {
+
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+    
+}
